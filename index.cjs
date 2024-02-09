81fa0108b4ab0ea6b6d42871d77e655d658386b3
ResumeByIng <142429826+ResumeByIng@users.noreply.github.com>
2024-02-09 22:48:42 +0700

    Update index.cjs

--- index.cjs
+++ index.cjs
@@ -721,7 +721,7 @@ app.post('/api/addAssessment', (req, res) => {
 
     const query = `
       INSERT INTO data_assessment ( class_year, course_code, name_professor, header, list_1, list_2, list_3, list_4, list_5, list_6, list_7, list_8, list_9, list_10 )
-      VALUES (?, ?, ?, ?, ?, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)
+      VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
     `;
 
     db.query(query, [class_year, course_code, name_professor, header, list_1, list_2, list_3, list_4, list_5, list_6, list_7, list_8, list_9, list_10], (error, results) => {
